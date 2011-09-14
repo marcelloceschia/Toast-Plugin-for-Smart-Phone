b6d10ada2522ccb011934c8d9874c6466d384d16
Dominic Bartl <domiii.1992@gmail.com>
2011-09-14 09:07:16 +0200

    Using BroadcastReceiver instead of Services

--- src/at/bartinger/toastplugin/PluginReceiver.java
+++ src/at/bartinger/toastplugin/PluginReceiver.java
@@ -1,43 +1,41 @@
 package at.bartinger.toastplugin;
 
-import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.widget.Toast;
 
-public class PluginService extends IntentService{
+public class PluginReceiver extends BroadcastReceiver{
 
-	public PluginService() {
-		super("PluginService");
-	}
 
 	private Handler handler;
 
 
 	@Override
-	protected void onHandleIntent(Intent intent) {
+	public void onReceive(final Context context, Intent intent) {
 
 		if(intent.hasExtra(SmartPhonePluginHelper.SMART_PHONE_PLUGIN_DATA)){
-			
+
 			final String data = intent.getStringExtra(SmartPhonePluginHelper.SMART_PHONE_PLUGIN_DATA);
-			
+
 			//is this a call to toggle your settings back?
 			final boolean toggleBack = intent.getBooleanExtra(SmartPhonePluginHelper.SMART_PHONE_PLUGIN_TOGGLE_BACK, false);
-			
-			
+
+
 			/**Put your stuff here*/
 			handler = new Handler(Looper.getMainLooper());
 			handler.post(new Runnable() {
 
 				public void run() {
-					Toast.makeText(getApplicationContext(), data + " Is toggleBack: " + toggleBack, Toast.LENGTH_LONG).show();
+					Toast.makeText(context, data + " Is toggleBack: " + toggleBack, Toast.LENGTH_LONG).show();
 				}
 			});
 			/*********************/
-			
-		}
-	}
 
+		}
+
+	}
+	
 }
